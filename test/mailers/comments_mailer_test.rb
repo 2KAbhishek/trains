2922c25a62a03f078b52724a63d5166a46c25afe
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-01-29 02:11:39 +0530

    Fix mailer test

--- test/mailers/comments_mailer_test.rb
+++ test/mailers/comments_mailer_test.rb
@@ -2,11 +2,9 @@ require "test_helper"
 
 class CommentsMailerTest < ActionMailer::TestCase
   test "submitted" do
-    mail = CommentsMailer.submitted
-    assert_equal "Submitted", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = CommentsMailer.submitted comments(:one)
+    assert_equal "New comment on the site", mail.subject
+    assert_match "comment", mail.body.encoded
   end
 
 end
