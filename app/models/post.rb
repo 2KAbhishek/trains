18f6520a6104365f455f2948f0a5b6bc216f7199
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-01-29 00:42:17 +0530

    Add comments support

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates_presence_of :title, :content
   validates_length_of :title, :minimum => 2
   has_rich_text :content
+  has_many :comments, dependent: :destroy
 end
