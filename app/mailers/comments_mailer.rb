88a0e60933fe3f111c02b29240fd0fdf0dc0ec1f
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-01-29 01:14:33 +0530

    Add mailer for comment submitted

---
+++ app/mailers/comments_mailer.rb
@@ -0,0 +1,13 @@
+class CommentsMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.comments_mailer.submitted.subject
+  #
+  def submitted(comment)
+    @comment = comment
+
+    mail to: ENV['ADMIN_EMAIL'], subject: 'New comment on the site'
+  end
+end
