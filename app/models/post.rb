d3b107d4bd753bcc06742ade41eed4d2333bbf50
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-06-08 11:46:20 +0530

    Add after_find translate hook

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,13 @@
 class Post < ApplicationRecord
+  after_find :translate
   validates_presence_of :title
   validates_length_of :title, minimum: 2
   has_rich_text :content
   has_many :comments, dependent: :destroy
+
+  private
+  def translate
+    self.title = I18n.t("posts.title")
+    Rails.logger.info "\n>>> Translating post #{title} <<<\n"
+  end
 end
